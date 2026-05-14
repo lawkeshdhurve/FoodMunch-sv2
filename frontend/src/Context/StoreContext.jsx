e08b58380bb935314404d91a528a0bf1bd5ba0fe
lawkeshdhurve <2022bit005@sggs.ac.in>
2026-05-14 16:56:56 +0530

    Update API URLs to live Render backend

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -8,7 +8,7 @@ const StoreContextProvider =(props)=>{
 
     
     const [cartItems,setCartItems] = useState({});  
-    const url = "http://localhost:4000"
+    const url = "https://foodmunch-backend-lqop.onrender.com"
     const [token,setToken]=useState("")
     const [food_list,setFoodList]=useState([]);
 

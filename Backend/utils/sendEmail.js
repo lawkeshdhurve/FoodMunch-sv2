ffea4f7f916b4bf0e7a8e3e0470961743445d525
lawkeshdhurve <2022bit005@sggs.ac.in>
2026-05-14 22:23:34 +0530

    update something

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -4,11 +4,16 @@ import nodemailer from "nodemailer";
 // so that process.env vars are read AFTER dotenv has loaded them.
 const createTransporter = () =>
   nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    family: 4,
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
   });
 
 export const sendVerificationEmail = async (to, token) => {
